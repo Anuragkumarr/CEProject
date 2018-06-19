73ec977219397fca490008872fe619ba4075e783
Ashish <amistry@cloudpipe.ca>
2018-06-19 14:50:07 -0400

    testcommit4

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ export default class App extends Component<Props> {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to mobile world !
+          Welcome to new world !
         </Text>
         <Text style={styles.instructions}>
           To get started, edit App.js
